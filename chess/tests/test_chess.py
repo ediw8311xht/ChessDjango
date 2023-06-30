3db3788454688019a13e98aeaf71c65df2ff7b01
ediw8311xht <maxballard333@gmail.com>
2023-06-30 14:23:34 -0400

    Z

--- chess/tests/test_chess.py
+++ chess/tests/test_chess.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 from django.test import TestCase
-from ..ChessLogic import *
+from ..ChessLogic.ChessBase import *
 
 class ChessLogicTestCase(TestCase):
     def setUp(self):
+        self.ggame = Game()
+
+    def test_game(self):
+        pass
+
+    def test_pawn(self):
+        pass
+
+    def test_queen(self):
         pass
         
     def tearDown(self):
