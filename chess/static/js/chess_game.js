d3663304ba86eba984e922e66f46ea1d66f9b2e6
ediw8311xht <maxballard333@gmail.com>
2023-07-06 03:49:13 -0400

    added front end stuff to chess_game, pretty/flip board

--- chess/static/js/chess_game.js
+++ chess/static/js/chess_game.js
@@ -1,29 +1,43 @@
 
-let piece_dict = { '-': 'empty', 'p':  'pawn', 'r':  'rook', 'n': 'knight', 'b': 'bishop', 'q': 'queen', 'k': 'king' };
+let pdict = { '-': 'empty', 'p':  'pawn', 'r':  'rook', 'n': 'knight', 'b': 'bishop', 'q': 'queen', 'k': 'king' };
 
 function lower(s) { return s.toLowerCase(); }
 
 function gcolor(s) { if (s == '-') { return 'empty'; } return lower(s) == s ? 'black' : 'white' }
 
-function piece_url(p) { let color = gcolor(p); return chess_url_image_base + color + '_' + piece_dict[lower(p)] + '.svg'; }
+function piece_url(p) { return chess_url_image_base + gcolor(p) + '_' + pdict[lower(p)] + '.svg' }
 
 function translate(y, x = null) {
-    return (typeof(y) == 'string' ?
-        [parseInt(y[1]), 'abcdefgh'.indexOf(y[0])]
-        : 'abcdefgh'[x] + x.toString());
+    return  ( typeof(y) == 'string'
+            ? [parseInt(y[1]), 'abcdefgh'.indexOf(y[0])]
+            : 'abcdefgh'[x] + x.toString() );
 }
 
-function get_square(y, x = null ) {
-    console.log(x);
-    if (typeof(y) == 'string') { return get_square(translate(y, x=x)); }
-    return document.getElementById('board-'+y+'-'+x);
+function get_square(y, x = null) {
+    return  ( typeof(y) == 'string'
+            ? get_square(translate(y, x=x))
+            : document.getElementById('board-'+y+'-'+x) );
 }
 
-function populate_board(ll) {
+function add_highlight() {
+    if (piece.classList.contains("highlight-piece")) {
+        piece.remove
+    }
+    else {
+    }
+}
+
+function populate_board(ll, rev = false, to_move = false) {
+    if (rev) { populate_board(ll.reverse()) };
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            let purl = piece_url( ll[i][j] );
-            get_square(i+1, j+1).innerHTML = '<img class="chess-piece-image" src="' + purl + '">';
+            if (to_move && gcolor(ll[i][j]) == to_move) {
+                get_square(i+1, j+1).classList.add("to-move-piece");
+                //get_square(i+1, j+1).addEventListener("click", add_highlight);
+            } else {
+                get_square(i+1, j+1).classList.remove("to-move-piece");
+            }
+            get_square(i+1, j+1).innerHTML = '<img class="chess-piece-image" src="' + piece_url( ll[i][j] ) + '">';
         }
     }
 }
@@ -35,9 +49,17 @@ function get_id(p_id) {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    let game_info  = JSON.parse(document.getElementById("game-get-info").textContent);
-    let game_board = game_info.board.split("\n");
-    populate_board(game_board);
+    let flip_board_button = document.getElementById("flip-board");
+    let game_info         = JSON.parse(document.getElementById("game-get-info").textContent);
+    let game_board        = game_info.board.split("\n").reverse();
+    let to_move           = game_info.to_move;
+    let ord               = false;
+
+    populate_board(game_board, rev=false, to_move=to_move);
     console.log(game_info);
+
+    flip_board_button.addEventListener("click", (event) => {
+        populate_board(game_board, rev=(! ord), to_move = to_move);
+    });
 });
 
